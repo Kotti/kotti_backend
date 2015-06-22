69f93ad97e6d2cadc81b0ee50645543e70330789
davidemoro <davide.moro@gmail.com>
2015-06-22 11:52:27 +0200

    remove resources import (wiped out)

--- kotti_backend/tests/conftest.py
+++ kotti_backend/tests/conftest.py
@@ -12,8 +12,6 @@ from pytest import fixture
 
 @fixture(scope='session')
 def custom_settings():
-    import kotti_backend.resources
-    kotti_backend.resources  # make pyflakes happy
     return {
         'kotti.configurators': 'kotti_tinymce.kotti_configure '
                                'kotti_backend.kotti_configure'}
