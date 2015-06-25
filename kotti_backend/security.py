eba80dfe07c3b0deb5fc818e56595fceb8bcfb24
davidemoro <davide.moro@gmail.com>
2015-06-25 06:18:37 +0200

    add pview permission (pview), enabled in simple_backend workflow for backend decoupled from the frontend

--- kotti_backend/security.py
+++ kotti_backend/security.py
@@ -3,7 +3,8 @@
 # that this is only really useful if you're _not_ using workflow.  If
 # you are, then you should look at the permissions in workflow.zcml.
 SITE_ACL = [
-    # [ 'Allow', 'system.Everyone', ['view']],
+    # ['Allow', 'system.Everyone', ['view']],
+    ['Allow', 'system.Everyone', ['pview']],
     ['Allow', 'role:viewer', ['view']],
     ['Allow', 'role:editor', ['view', 'add', 'edit', 'state_change']],
     ['Allow', 'role:owner', ['view', 'add', 'edit', 'manage', 'state_change']],
