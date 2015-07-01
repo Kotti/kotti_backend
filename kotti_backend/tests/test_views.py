78678b3b9d8e3a9fb27190c1cfcc800f1938a69a
davidemoro <davide.moro@gmail.com>
2015-07-02 00:11:45 +0200

    removed unusued imports from tests and add another test

--- kotti_backend/tests/test_views.py
+++ kotti_backend/tests/test_views.py
@@ -4,12 +4,18 @@ from pytest import mark
 class TestViews:
 
     @mark.user('admin')
-    def test_root_view_admin(self, webtest, root):
+    def test_root_view_admin(self, webtest):
         # admin can view
         resp = webtest.get('/')
         assert resp.status_code == 200
 
-    def test_root_view(self, webtest, root):
+    def test_root_view(self, webtest):
         # view requires authentication
         resp = webtest.get('/')
         assert resp.status_code == 302
+
+    @mark.user('admin')
+    def test_root_view_admin_default(self, webtest):
+        # admin can view
+        resp = webtest.get('/')
+        assert '/@@contents' not in resp.body
